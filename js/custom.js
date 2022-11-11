6d23f6364d37777f4828e609b844f8fdbae8e6d4
freeman <iviewn@gmail.com>
2022-11-11 20:36:59 +0300

    index

--- js/custom.js
+++ js/custom.js
@@ -29,30 +29,38 @@ $(document).ready(function() {
 
 
 
-    function scrollEvent() {
-        var hT = $('.vote').offset().top,
-          hH = $('.vote').outerHeight(),
-          wH = $(window).height(),
-          wS = $(this).scrollTop();
-        if (wS > (hT+hH-wH)){
-            $('.vote__like div').each(function () {
-                $(this).prop('Counter',0).animate({
-                    Counter: $(this).text()
-                }, {
-                    duration: 2000,
-                    easing: 'swing',
-                    step: function (now) {
-                        $(this).text(Math.ceil(now));
-                    }
-                });
-            });
-          window.removeEventListener("scroll", scrollEvent);
-        }
-    }
-    window.addEventListener("scroll", scrollEvent);
+    // function scrollEvent() {
+    //     var hT = $('.vote').offset().top,
+    //       hH = $('.vote').outerHeight(),
+    //       wH = $(window).height(),
+    //       wS = $(this).scrollTop();
+    //     if (wS > (hT+hH-wH)){
+    //         $('.vote__like div').each(function () {
+    //             $(this).prop('Counter',0).animate({
+    //                 Counter: $(this).text()
+    //             }, {
+    //                 duration: 2000,
+    //                 easing: 'swing',
+    //                 step: function (now) {
+    //                     $(this).text(Math.ceil(now));
+    //                 }
+    //             });
+    //         });
+    //       window.removeEventListener("scroll", scrollEvent);
+    //     }
+    // }
+    // window.addEventListener("scroll", scrollEvent);
 
-
-
+    $('#slick_1').slick({
+        slidesToShow: 1,
+        fade: true,
+        autoplay: true
+    });
+    $('#slick_2').slick({
+        slidesToShow: 1,
+        fade: true,
+        autoplay: true
+    });
 
 
 
